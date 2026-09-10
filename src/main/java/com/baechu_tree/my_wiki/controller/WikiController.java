0cd307bd4e94820287634b056794d234b2d1cd43
baechuTree <jt34175413@gmail.com>
2026-09-10 15:31:03 +0900

    개발: WikiController, temp_error.html: 컨트롤러에 문서 CRUD 요청을 받는 링크에 맞는 메서드 추가(아직 내용은 미완성) 및 임시 공용 에러 페이지 제작

--- src/main/java/com/baechu_tree/my_wiki/controller/WikiController.java
+++ src/main/java/com/baechu_tree/my_wiki/controller/WikiController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class WikiController {
@@ -21,7 +23,7 @@ public class WikiController {
     }
 
     @GetMapping(WikiPaths.PATH_DOCUMENT_LIST)
-    public String DocumentList(Model model) {
+    public String DocumentListPage(Model model) {
         List<TitleAndRoute> titlesAndPaths = new ArrayList<>();
 
         List<WikiDocument> allDocuments = documentService.findAll();
@@ -30,13 +32,59 @@ public class WikiController {
         }
 
         model.addAttribute("titlesAndPaths", titlesAndPaths);
+
         return "document_list";
     }
 
     @GetMapping(WikiPaths.PATH_DOCUMENT_DETAIL)
-    public String DocumentDetail(Model model, @PathVariable String documentTitle) {
-        model.addAttribute("documentTitle", documentTitle);
+    public String DocumentDetailPage(Model model, @PathVariable String documentTitle) {
+        Optional<WikiDocument> documentOptional = documentService.findByTitle(documentTitle);
+
+        if (documentOptional.isEmpty()) return "temp_error"; // TODO: 제대로 된 에러 처리 필요!
+
+        WikiDocument document = documentOptional.get();
+
+        model.addAttribute("documentTitle", document.getDocumentTitle());
+        model.addAttribute("documentArticle", document.getContent());
+
         return "document_detail";
+    }
+
+    @GetMapping("/wiki/save_page")
+    public String DocumentSavePage(Model model) {
+        return "document_save";
+    }
+
+    @PostMapping("wiki/save")
+    public String DocumentSave(Model model) {
+        // TODO: 문서 세이브 로직 완성 필요!
+        return null;
+    }
+
+    @GetMapping("/wiki/update_page")
+    public String DocumentUpdatePage(Model model, @PathVariable String documentTitle) {
+
+        Optional<WikiDocument> documentOptional = documentService.findByTitle(documentTitle);
+
+        if (documentOptional.isEmpty()) return "temp_error"; // TODO: 제대로 된 에러 처리 필요!
+
+        WikiDocument document = documentOptional.get();
+
+        model.addAttribute("originalDocumentArticle", document.getContent());
+
+        return "document_update";
+    }
+
+    @PostMapping("wiki/update")
+    public String DocumentUpdate(Model model) {
+        // TODO: 문서 업데이트 로직 완성 필요!
+        return null;
+    }
+
+    @PostMapping("wiki/delete")
+    public String DocumentDelete(Model model) {
+        // TODO: 문서 삭제 로직 완성 필요!
+        return null;
     }
 
     private String GetPathOfSpecificDocumentDetail(String documentTitle) {
